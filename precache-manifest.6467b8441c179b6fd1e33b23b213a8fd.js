df6e58c632970c1dea3bebb9a9bfb23aabe054a6
sorcererxw <tujipqy@gmail.com>
2019-05-31 20:02:15 +0800

    Updates

---
+++ precache-manifest.6467b8441c179b6fd1e33b23b213a8fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a3bb6bd6c7edd28eb0636c54f7bfec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0733a471320d38a6a760",
+    "url": "/static/css/main.7ba7445a.chunk.css"
+  },
+  {
+    "revision": "b28646c0f8bff8ffaa8e",
+    "url": "/static/js/2.8a947b89.chunk.js"
+  },
+  {
+    "revision": "0733a471320d38a6a760",
+    "url": "/static/js/main.48facc31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
